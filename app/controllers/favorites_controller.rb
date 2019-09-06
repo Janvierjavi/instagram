4efdffc9523b6aa942637a3a95323db2b83302d2
janvierjavim <janvierjavim2@gmail.com>
2019-09-05 22:40:45 -0700

    set the root for localhost and fix the image size

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,7 +6,7 @@ class FavoritesController < ApplicationController
     
       def destroy
         favorite = current_user.favorites.find_by(id: params[:id]).destroy
-        redirect_to posts_url, notice: "#{favorite.post.user.First_Name}'s post has been unfavored"
+        redirect_to posts_url, notice: "post has been unfavored"
       end
       def index
         @favorites = Favorite.all
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
-  validates :Email,uniqueness: true, length: { maximum: 30 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, 
-  validates :First_Name, presence: true
+  validates :Email,uniqueness: true, length: { maximum: 30 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :First_Name,  presence: true, length: { maximum: 19 }
   has_many :posts
   has_one :profile
   has_many :favorites, dependent: :destroy
